34ed0ee865df4c654a5cf6f2f7bb66e25b2b2053
Dafisto <chrisreid2002@gmail.com>
2024-08-14 14:56:22 -0600

    -web pages

--- LibraryOfAlexandria/ClassPackage/Persistence/DataBase.cs
+++ LibraryOfAlexandria/ClassPackage/Persistence/DataBase.cs
@@ -8,62 +8,61 @@ using System.Threading.Tasks;
 namespace LibraryOfAlexandria.ClassPackage.Persistence
 { 
     internal class DataBase
-
     {
-    private readonly SqlConnection _database;
+        public SqlConnection connection;
      
 
-    //constructor for database connection
-    public DataBase()
-    {
-        Initialize();
-    }
-    public void Initialize()
-    {
-        
-
-        connection = new SqlConnection(connectionString);
-    }
-    //This will open a connection to the library database
-    public bool OpenConnection()
-    {
-        try
+        //constructor for database connection
+        public DataBase()
         {
-            connection.Open();
-            return true;
+            Initialize();
         }
-        catch (MySqlException ex)
+        public void Initialize()
         {
-            //When handling errors, you can your application's response based 
-            //on the error number.
-            //The two most common error numbers when connecting are as follows:
-            //0: Cannot connect to server.
-            //1045: Invalid user name and/or password.
-            switch (ex.Number)
+            var dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../../..", "resources", "database", "alexandriaDataBase.db");
+            connection = new SqlConnection(dbPath);
+        }
+        //This will open a connection to the library database
+        public bool OpenConnection()
+        {
+            try
             {
-                case 0:
-                    Console.WriteLine("Cannot connect to server.  Contact administrator");
-                    break;
-
-                case 1045:
-                    Console.WriteLine("Invalid username/password, please try again");
-                    break;
+                connection.Open();
+                return true;
             }
-            return false;
+            catch (SqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
         }
-    }
-    //this will close the connection to the library data base
-    public bool CloseConnection()
-    {
-        try
+        //this will close the connection to the library data base
+        public bool CloseConnection()
         {
-            connection.Close();
-            return true;
-        }
-        catch (MySqlException ex)
-        {
-            Console.WriteLine(ex.Message);
-            return false;
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
     }
 }
